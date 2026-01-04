b4f763634b4c6bd7f07c458cde1f6ee0c5f4d944
triptishakya-dev <triptishakya.2001@gmail.com>
2026-01-04 14:03:41 +0530

    feat: Initialize Next.js project with Clerk authentication, Shadcn UI, and basic application structure.

---
+++ client/src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+   <div className='min-h-[93vh] w-full flex justify-center items-center'>
+    <SignIn  />
+   </div>
+  )
+}
