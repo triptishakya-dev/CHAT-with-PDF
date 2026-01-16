6c5ab0519ca27674c05140f4ea6b6f591fd48ff8
triptishakya-dev <triptishakya.2001@gmail.com>
2026-01-16 23:38:55 +0530

    feat: Add Clerk authentication middleware and implement server-side PDF upload with queuing and chat API.

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, `${uniqueSuffix}-${file.originalname}`);
   },
-});
+});8
 
 const upload = multer({ storage: storage });
 const app = express();
@@ -31,10 +31,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload/pdf", upload.single("pdf"), async (req, res) => {
+  console.log("api is hit")
   await queue.add(
     "file-ready",
     JSON.stringify({
-      filename: req.file.originalname,
       destination: req.file.destination,
       path: req.file.path,
     })
